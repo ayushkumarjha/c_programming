b9856cf0de6f39f6539661d9c68da6942356e916
Ayush Kumar Jha <84013088+shyamdevelopers@users.noreply.github.com>
2021-05-23 14:38:23 +0530

    substraction.c
    it will substract the two digits

---
+++ Substraction.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	int a , b , sub;
+	printf("Please enter the first digit:");
+	scanf("%d" , &a);
+	printf("Please enter the second digit:");
+	scanf("%d" , &b);
+	sub = a-b;
+	printf("Subtraction of both no.: %d" , sub);
+	return 0;
+}
