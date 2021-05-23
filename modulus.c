c0595039dd7ac75b97c9ca476b81fdec2d309496
Ayush Kumar Jha <84013088+shyamdevelopers@users.noreply.github.com>
2021-05-23 15:19:56 +0530

    modulus.c
    it will calculated the modulus..
    **modulus means it is equal to zero..

---
+++ modulus.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	int a,b,mod;
+	printf("please enter the first digit:");
+	scanf("%d",&a);
+	printf("please enter the second digigt:");
+	scanf("%d",&b);
+	mod = a%b;
+	printf("modulus:%d",mod);
+	return 0;
+}
