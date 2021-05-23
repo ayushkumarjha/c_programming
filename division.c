2b9dfae1025f74bf8d47d5aa9ca4e533098404ad
Ayush Kumar Jha <84013088+shyamdevelopers@users.noreply.github.com>
2021-05-23 14:52:09 +0530

    division.c
    It will divided the two digits.............

---
+++ division.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	float a,b,div;
+	printf("Please enter the first digit:");
+	scanf("%f",&a);
+	printf("Pleasec enter the second digit:");
+	scanf("%f",&b);
+	div=a/b;
+	printf("Divided Value:%f",div);
+	return 0;
+}
