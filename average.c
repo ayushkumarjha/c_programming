b9374409e8cb6b664808eef9ba02d6d3e07b2b6f
Ayush Kumar Jha <84013088+shyamdevelopers@users.noreply.github.com>
2021-05-23 20:26:21 +0530

    average.c
    It will calculate the average of your two different digits.....

---
+++ average.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c;
+	float avg;
+	printf("Please enter the first digit:");
+	scanf("%d", &a);
+	printf("Please enter the second digit:");
+	scanf("%d", &b);
+	avg=(a+b)/2;
+     printf("Here the average value of yiur digit:%f",avg);
+     return 0;
+	
+}
