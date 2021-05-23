4f858d04654204ba70e6055bd47d941543bebbf4
Ayush Kumar Jha <84013088+shyamdevelopers@users.noreply.github.com>
2021-05-23 23:06:12 +0530

    ascii.c
    It will shows your ascii value ................

---
+++ ascii.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char b;
+	int a;
+	printf("Please put your Digit(for_checking_the_ASCII_Value):");
+	scanf("%c",&b);
+	a=b;
+	printf("Here's your ASCII value:%d",b);
+	return 0;
+}
