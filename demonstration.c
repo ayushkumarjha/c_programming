15d9f2437f659b627abd685e742925713700d431
Ayush Kumar Jha <84013088+shyamdevelopers@users.noreply.github.com>
2021-05-23 23:16:55 +0530

    demonstration.c
    it will demonstrated u to in c \,\n,\t\b how they work..........

---
+++ demonstration.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+     
+    printf("Hello\nWorld!");    //use of \n
+    printf("\nhere it will show u the use of slash_n");
+    printf("\nHello\tWorld!");  // use of \t
+    printf("\nhere it will show u the use of slash_t");
+    printf("\n\"Hello World!\"");   //use of \"
+    printf("\nhere it will show u the use of slash");
+    printf("\nHello\bWorld!");      //use of \b
+    printf("\nhere it will show u the use of slash_b");
+    return 0;
+}
