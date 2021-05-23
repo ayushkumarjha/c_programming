e7fff1d5f38cd9cee77929304dd92a59db1e3639
Ayush Kumar Jha <84013088+shyamdevelopers@users.noreply.github.com>
2021-05-23 14:45:11 +0530

    multiply.c
    it will multiplied both the no.

---
+++ multiply.c
@@ -0,0 +1,13 @@
+#include <Stdio.h>
+#include <conio.h>
+void main()
+{
+	int a,b,multiply;
+	printf("Please enter your first digit:");
+	scanf("%d",&a);
+	printf("Please enter your second digit:");
+	scanf("%d",&b);
+	multiply=a*b;
+	printf("Multiplied Both no.:%d",multiply);
+	return 0;
+}
