01ce7dfda196bbf885befd7f7f021e391f5b66f2
Ayush Kumar Jha <84013088+shyamdevelopers@users.noreply.github.com>
2021-05-24 10:44:46 +0530

    areaofrectangle.c
    It will calculate the area of rectangle.....................

---
+++ areaofrectangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main() 
+{
+	int a,b,c;
+	printf("Please enter the length of an rectangle:");
+	scanf("%d", &a);
+	printf("Please enter the breadth of an rectangle:");
+	scanf("%d", &b);
+	c=a*b;
+	printf("Here's your area of rectangle is :%d",c);
+	return 0;
+}
