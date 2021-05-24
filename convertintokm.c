a387fd15675c6e1062de9a7084158be8c102e55e
Ayush Kumar Jha <84013088+shyamdevelopers@users.noreply.github.com>
2021-05-24 12:23:08 +0530

    convertintokm.c

---
+++ convertintokm.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   float km,m,f,i;
+   printf("Please enter the distance b/w two cities in km:");
+   scanf("%f",&km);
+   m=km*1000;
+   printf("\nmeter:%f",m);
+   f=m*3;
+   printf("\nfoot:%f",f);
+   i=f*2.54;
+   printf("\ninches:%f",i);
+   return 0;
+}
