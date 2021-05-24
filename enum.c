2a06e977b77768c8e7306e0bc642a29d46e7f948
Ayush Kumar Jha <84013088+shyamdevelopers@users.noreply.github.com>
2021-05-24 19:10:34 +0530

    enum.c

---
+++ enum.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<conio.h>
+enum team{class=5, student=50, teacher=10};
+int main() {
+   printf("The value of enum team: %d\t%d\t%d",class, student, teacher);
+   return 0;
+}
