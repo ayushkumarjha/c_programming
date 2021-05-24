e666fa0084b83daee0a6f1d6aefa703fcbe60e36
Ayush Kumar Jha <84013088+shyamdevelopers@users.noreply.github.com>
2021-05-24 18:29:01 +0530

    aggregate.c
    it will helps u to calculate your results...........

---
+++ aggregate.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int maths,physics,english,hindi,ip,chemistry;
+	float a;
+	printf("Please Enter your Mathematics marks:");
+	scanf("%d",&maths);
+	printf("Please Enter your Chemistry marks:");
+	scanf("%d",&chemistry);
+	printf("Please Enter your Physics marks:");
+	scanf("%d",&physics);
+	printf("Please Enter your Hindi marks:");
+	scanf("%d",&hindi);
+	printf("Please Enter your English marks:");
+	scanf("%d",&english);
+	printf("Please Enter your IP/PHE marks:");
+	scanf("%d",&ip);
+	a=(maths+chemistry+physics+hindi+english+ip)*100/500;
+	printf("Here's your percantage result in:%f",a);
+	return 0;
+}
