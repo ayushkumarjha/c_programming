9c280dd624b6a3a17fc0fcc25488a1a91555bc08
Ayush Kumar Jha <84013088+shyamdevelopers@users.noreply.github.com>
2021-05-28 15:19:31 +0530

    absolute.c

---
+++ absolute.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	//absolute means modulus|-1|=1 of any number
+	int i;
+	printf("Please enter a number which you want to check:");
+	scanf("%d",&i);
+	if(i<0)
+	{
+	i*=-1;
+	printf("Her's your absoulte value:%d",i);
+}
+else
+{
+	printf("Here's your absoulte value:%d",i);
+	}
+	printf("\nThank you!\nIf u got your result please press enter & u may exit..");	
+getch();
+}
