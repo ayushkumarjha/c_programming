7019aceb8961fd60b6fc2854a36f7b20417811bf
Ayush Kumar Jha <84013088+shyamdevelopers@users.noreply.github.com>
2021-05-28 16:52:39 +0530

    week_if_else.c

---
+++ week_if_else.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num;
+	printf("Please put only positive natural number here:");
+	scanf("%d",&num);
+	if(num==1)
+	printf("Hey!Dude Today's is Monday..");
+	else
+	if(num==2)
+	printf("Hey!Dude Today's is Tuesday..");
+	else
+	if(num==3)
+	printf("Hey!Dude Today's is Wednesday..");
+	else
+	if(num==4)
+	printf("Hey!Dude Today's is Thursday..");
+	else
+	if(num==5)
+	printf("Hey!Dude Today's is Friday..");
+	else
+	if(num==6)
+	printf("Hey!Dude Today's is Saturday..");
+	else
+	if(num==7)
+	printf("Hey!Dude Today's is Sunday..");
+	else
+	printf("Hey!Dude you Don't known this thing that in a week only 7 days are present....");
+	printf("\nThank you!\nIf u got your result please press enter & u may exit..");	
+    getch();
+    return 0;
+	
+}
