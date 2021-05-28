f2c5444657acd29bf229a93b478b3e9061035c4f
Ayush Kumar Jha <84013088+shyamdevelopers@users.noreply.github.com>
2021-05-28 21:56:30 +0530

    electricitybill.c

---
+++ electricitybill.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	float bill, unit;
+
+	printf("Enter the units consumed=");
+	scanf("%f",&unit);
+
+	if(unit<100)
+	{
+		bill=unit*2.25;
+		printf("Electricity Bill=%f Rupees",bill);
+	}
+	else if(unit>=100 && unit<200)
+	{
+		bill=unit*3.00;
+		printf("Electricity Bill=%f Rupees",bill);
+
+	}
+	else if(unit>=200 && unit<500)
+	{
+		bill=unit*4.25;
+		printf("Electricity Bill=%f Rupees",bill);
+
+	}
+
+	else if(unit>=500)
+	{
+		bill=unit*7.00;
+		printf("Electricity Bill=%f Rupees",bill);
+
+	}
+	else
+	{
+		printf("Please enter valid consumed units...");
+	}
+	return 0;
+}
