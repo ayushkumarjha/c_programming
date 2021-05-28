2cca7e4cf63c19d61fc842bcef9592db3e26c8a0
Ayush Kumar Jha <84013088+shyamdevelopers@users.noreply.github.com>
2021-05-28 15:29:36 +0530

    positive&negative.c

---
+++ positive&negative.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	long int i;
+	printf("Please put your digit/number for your result:");
+	scanf("ld",&i);
+	if(i>=0)
+	printf("Hey Dude! your input is Positive");
+	else
+	printf("Hey Dude! your input is Positive");
+	printf("\nThank you!\nIf u got your result please press enter & u may exit..");	
+    getch();
+    return 0;
+}
