b4440b1709744acbdcc0537e39b02ab6fc54d4c9
Ayush Kumar Jha <84013088+shyamdevelopers@users.noreply.github.com>
2021-05-28 15:46:25 +0530

    maximum2.c

---
+++ maximum2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,j;
+	printf("Please Put your First Digit/Number:");
+	scanf("%d",&i);
+	printf("Please Put your second Digit/Number:");
+	scanf("%d",&j);
+	if(i>j)
+	printf("Hey Dude! Your maximum Digit/Number is:%d",i);
+	else
+	if(j>i)
+	printf("Hey Dude! Your maximum Digit/Number is:%d",j);
+	else
+	printf("Hey Dude! Your Digit/Number is Same");
+	printf("\nThank you!\nIf u got your result please press enter & u may exit..");	
+    getch();
+    return 0;	
+}
