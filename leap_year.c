0d64dbcd58a171d6c3843a477aba6a82058413fd
Ayush Kumar Jha <84013088+shyamdevelopers@users.noreply.github.com>
2021-05-28 17:07:15 +0530

    leap_year.c

---
+++ leap_year.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num;
+	printf("Please Enter a Year:");
+	scanf("%d",&num);
+	if (num%4==0)
+	printf("Hey!Dude It's an Leap Year");
+	else
+	printf("Hey!Dude It's an Sidereal Year");
+	printf("\nThank you!\nIf u got your result please press enter & u may exit..");	
+    getch();
+    return 0;
+}
