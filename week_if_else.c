87406451db6dc955e6cd142313a8322c482b4554
Ayush Kumar Jha <84013088+shyamdevelopers@users.noreply.github.com>
2021-05-29 16:51:05 +0530

    week_if_else.c

--- week_if_else.c
+++ week_if_else.c
@@ -10,25 +10,18 @@ void main()
 	else
 	if(num==2)
 	printf("Hey!Dude Today's is Tuesday..");
-	else
-	if(num==3)
+	else if(num==3)
 	printf("Hey!Dude Today's is Wednesday..");
-	else
-	if(num==4)
+	else if(num==4)
 	printf("Hey!Dude Today's is Thursday..");
-	else
-	if(num==5)
+	else if(num==5)
 	printf("Hey!Dude Today's is Friday..");
-	else
-	if(num==6)
+	else if(num==6)
 	printf("Hey!Dude Today's is Saturday..");
-	else
-	if(num==7)
+	else if(num==7)
 	printf("Hey!Dude Today's is Sunday..");
 	else
 	printf("Hey!Dude you Don't known this thing that in a week only 7 days are present....");
 	printf("\nThank you!\nIf u got your result please press enter & u may exit..");	
-    getch();
-    return 0;
-	
+    getch();	
 }
