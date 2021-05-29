f7ec22a97d62e49f687f7091baa65f5d900d068c
Ayush Kumar Jha <84013088+shyamdevelopers@users.noreply.github.com>
2021-05-29 16:44:10 +0530

    maximum2.c

--- maximum2.c
+++ maximum2.c
@@ -9,12 +9,10 @@ void main()
 	scanf("%d",&j);
 	if(i>j)
 	printf("Hey Dude! Your maximum Digit/Number is:%d",i);
-	else
-	if(j>i)
+	else if(j>i)
 	printf("Hey Dude! Your maximum Digit/Number is:%d",j);
 	else
 	printf("Hey Dude! Your Digit/Number is Same");
 	printf("\nThank you!\nIf u got your result please press enter & u may exit..");	
     getch();
-    return 0;	
 }
